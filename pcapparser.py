78d770337455171e42bb3406f8f6ab665b7044b2
sea0breeze <sea0breeze@users.noreply.github.com>
2016-03-23 14:18:21 +0800

    Update pcapparser.py
    Add support for gzip decompression. More bugs about following the tcp stream still need fixing.

--- pcapparser.py
+++ pcapparser.py
@@ -2,8 +2,16 @@
 import argparse
 import dpkt
 import urllib
+import gzip
+import StringIO
 
 DEBUG = False
+
+def gzdecode(data) :
+    compressedstream = StringIO.StringIO(data)
+    gziper = gzip.GzipFile(fileobj=compressedstream)
+    data2 = gziper.read()
+    return data2
 
 def dictappend(d, k, v):
     if k in d:
@@ -17,10 +25,12 @@ def ipparse(ipstr):
     return '{}.{}.{}.{}'.format(ip[0], ip[1], ip[2], ip[3])
 
 def show(enermy, httplist):
+    #return
     httpdirt = {}
     for seq, http in httplist:
         httpdirt[seq] = http
     for seq in sorted(httpdirt.keys()):
+        print seq
         http = httpdirt[seq]
         print '*******************************************************************'
         if type(http) == dpkt.http.Request:
@@ -36,7 +46,16 @@ def show(enermy, httplist):
             for i in header:
                 print '{}: {}'.format(i, header[i])
         print '\n'
-        print repr(urllib.unquote_plus(http.body))
+        if http.body != None:        # Avoid error when body is empty.
+            if http.headers.get('content-encoding') == 'gzip':
+                try:
+                    http.body = gzdecode(http.body)
+                except Exception as e:
+                    print e;
+            if DEBUG :
+                print len(http.body)
+            else:
+                print repr(urllib.unquote_plus(http.body))
         print '\n\n*******************************************************************'
 
 
@@ -52,8 +71,8 @@ parser.add_argument('-p', '--port', type=int, default=80,
 parser.add_argument('-i', '--ip', help='spcify the ip address which you think is evil and may steal your flag')
 args = parser.parse_args()
 
-reqtmp = ''
-restmp = ''
+reqtmp = {}
+restmp = {}
 httppool = {}
 pcap = dpkt.pcap.Reader(args.pcapfile)
 for ts, buf in pcap:
@@ -68,39 +87,52 @@ for ts, buf in pcap:
         tcp = ip.data
         if tcp.dport == args.port and len(tcp.data) > 0:
             source = (ip.src, tcp.sport)
-            reqtmp += tcp.data
+            if tcp.seq in [i[0] for i in httppool.get(source, [])]:
+                continue
+            dictappend(reqtmp, source, tcp.data)
+            #if tcp.sport == 52833:
+            #    print repr(ip)+'\n\n'
             try:
-                req = dpkt.http.Request(reqtmp)
+                req = dpkt.http.Request(reqtmp[source])
                 if DEBUG:
-                    print repr(req)
-                if ((args.search in restmp) if args.search else True):
+                    print req.headers
+                if ((args.search in reqtmp[source]) if args.search else True):
                     dictappend(httppool, source, [(tcp.seq, req)])
-                reqtmp = ''
+                reqtmp[source] = ''
             except dpkt.UnpackError as e:
                 if DEBUG:
-                    print '{}:{}'.format(ipparse(source[0]), source[1]), 'length of segment: {}'.format(len(reqtmp))
-                    print e
-                if reqtmp[:3] != 'GET' and reqtmp[:4] != 'POST':
-                    reqtmp = ''   # The data in this TCP stream is not http!
+                    print '{}:{}'.format(ipparse(source[0]), source[1]),
+                    print 'length of request segment: {}'.format(len(reqtmp[source]))
+                    print ts, e
+                if reqtmp[source][:3] != 'GET' and reqtmp[source][:4] != 'POST':
+                    reqtmp[source] = ''   # The data in this TCP stream is not http!
 
         if tcp.sport == args.port and len(tcp.data) > 0:
             destination = (ip.dst, tcp.dport)
-            restmp += tcp.data
+            if tcp.seq in [i[0] for i in httppool.get(destination, [])]:
+                continue
+            dictappend(restmp, destination, tcp.data)
+            #if tcp.dport == 52833:
+            #    print repr(ip)+'\n\n'
             try:
-                res = dpkt.http.Response(restmp)
+                res = dpkt.http.Response(restmp[destination])
                 if DEBUG:
-                    print repr(res)
-                if ((args.search in restmp) if args.search else True):
+                    print res.headers
+                if ((args.search in restmp[destination]) if args.search else True):
                     dictappend(httppool, destination, [(tcp.seq, res)])
-                restmp = ''
+                restmp[destination] = ''
             except dpkt.UnpackError as e:
+                #if 'invalid' in str(e):
+                #    print repr(tcp), '{}:{}'.format(ipparse(destination[0]), destination[1])
                 if DEBUG:
-                    print '{}:{}'.format(ipparse(destination[0]), destination[1]), 'length of segment: {}'.format(len(restmp))
-                    print e
-                if restmp[:4] != 'HTTP':
-                    restmp = ''   # The data in this TCP stream is not http!
+                    print '{}:{}'.format(ipparse(destination[0]), destination[1]),
+                    print 'length of response segment: {}'.format(len(restmp[destination]))
+                    print ts, e
+                if restmp[destination][:4] != 'HTTP':
+                    restmp[destination] = ''   # The data in this TCP stream is not http!
 
 for ip, port in httppool:
-    if args.ip and ip != args.ip:
+    readableIP = ipparse(ip)
+    if args.ip and readableIP != args.ip:
         continue
-    show('{}:{}'.format(ipparse(ip), port), httppool[(ip, port)])
+    show('{}:{}'.format(readableIP, port), httppool[(ip, port)])
